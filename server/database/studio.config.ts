89958b1dd8ba026da96ba413765ebf2fe04f2623
svenvreenen <85950390+svenvreenen@users.noreply.github.com>
2025-02-14 22:17:54 +0100

    test

--- server/database/studio.config.ts
+++ server/database/studio.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   schema: './server/database/schema.ts',
-  driver: 'd1-http',
+  out: './server/database/migrations',
+  dialect: 'sqlite',
   dbCredentials: {
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID || '',
-    databaseId: process.env.CLOUDFLARE_DATABASE_ID || '',
-    token: process.env.CLOUDFLARE_API_TOKEN || ''
+    url: '.data/sqlite.db'
   },
   verbose: true,
-  strict: true,
-  dialect: 'sqlite'
+  strict: true
 }); 
